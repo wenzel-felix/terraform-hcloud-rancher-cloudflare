a84264cefac38b0bf0cb69fdb0865f37e6b7c462
Felix <felixwenzel0@gmail.com>
2023-07-05 23:30:59 +0200

    new provider and module version

--- cluster.tf
+++ cluster.tf
@@ -1,9 +1,10 @@
 module "rke2" {
-  source                      = "wenzel-felix/rke2/hcloud"
-  version                     = "0.1.1"
-  hetzner_token               = var.hetzner_token
-  master_node_count           = 3
-  worker_node_count           = 1
-  additional_lb_service_ports = ["80", "443"]
-  rke2_version                = var.rke2_version
+  source                               = "wenzel-felix/rke2/hcloud"
+  version                              = "0.3.0"
+  hetzner_token                        = var.hetzner_token
+  use_cluster_managed_tls_certificates = false
+  master_node_count                    = 3
+  worker_node_count                    = 1
+  additional_lb_service_ports          = ["80", "443"]
+  rke2_version                         = var.rke2_version
 }
--- examples/simple-setup/main.tf
+++ examples/simple-setup/main.tf
@@ -1,11 +1,12 @@
 module "rancher" {
-  source             = "../.."
-  hetzner_token      = var.hetzner_token
-  cloudflare_token   = var.cloudflare_token
-  cloudflare_zone_id = var.cloudflare_zone_id
-  cloudflare_domain = var.cloudflare_domain
+  source                       = "../.."
+  hetzner_token                = var.hetzner_token
+  cloudflare_token             = var.cloudflare_token
+  cloudflare_zone_id           = var.cloudflare_zone_id
+  cloudflare_domain            = var.cloudflare_domain
   management_master_node_count = 3
-  letsencrypt_issuer = var.letsencrypt_issuer
+  letsencrypt_issuer           = var.letsencrypt_issuer
+  rke2_version                 = "v1.24.15+rke2r1"
 }
 
 output "rancher_admin_username" {
